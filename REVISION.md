82612aa381b70f15ec67de20bbb3c686c8d36d35
Signe <signe.skold@hotmail.com>
2020-09-24 18:09:30 +0100

    Making improvements

---
+++ REVISION.md
@@ -0,0 +1,6 @@
+Revision history for jsramverk
+==============================
+
+v1.0.0 (2020-09-24)
+
+* Done with Frontend.
--- src/Reports.js
+++ src/Reports.js
@@ -7,8 +7,8 @@ return (
 <h1>Rapportsida i kursen jsramverk</h1>
 <p></p>
 
-<p>Klicka <a href="https://github.com/signeskold">här</a> för att komma till
-mitt kursrepo på Github eller <a href="../README.md">här</a> för att komma direkt
+<p>Klicka <a href="https://github.com/signeskold/me-app-jsramverk-frontend">här</a> för att komma till
+mitt kursrepo på Github eller <a href="https://github.com/signeskold/me-app-jsramverk-frontend/blob/master/README.md">här</a> för att komma direkt
 till min README.md-fil.</p>
 </main>
 );
